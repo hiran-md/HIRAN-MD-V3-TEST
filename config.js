aef53da21c5729cb55e7c79bef4bd961d631d90c
Hiranya Sathsara <hiranmdbot@gmail.com>
2025-03-18 20:02:24 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+module.exports = {
+  sessionId: 'yVM1UACJ#mOvLTScgfOi_J5BEv0DdCdYYdWavUaWkKYtPHqawZhc',  // Replace with your actual session ID
+  sessionFile: './auth_info.json',
+  botName: 'ʜɪʀᴀɴ ᴍᴅ ᴠ3',  // Bot name
+};
