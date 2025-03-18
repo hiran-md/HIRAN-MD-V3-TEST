54cfea0971813d5bb624e5a6cc8738b6751eb6d2
Hiranya Sathsara <hiranmdbot@gmail.com>
2025-03-18 20:04:47 +0530

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,19 @@
+// settings.js
+module.exports = {
+  prefix: '.',  // Command prefix, you can change it
+  botName: 'ʜɪʀᴀɴ ᴍᴅ ᴠ3',
+
+  // Menu options
+  menu: {
+    welcomeMessage: 'Welcome to the HIRAN MD V3! Please choose an option below:',
+    aliveMessage: 'Hey HIRAN MD V3 is alive and running smoothly.',
+    systemInfoMessage: 'Bot version 1.0.0, running on Node.js.'
+  },
+
+  downloadSettings: {
+    movieApi: 'https://cinesubz.mizta-x.com/movie-search?name=',
+  },
+
+  imageUrl: 'https://avatars.githubusercontent.com/u/187024208?v=4',
+  footerText: '• ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʜɪʀᴜᴡᴀ ᴛᴇᴄʜ',
+};
